6fe0cc3f70378c42fe037777e38ae629eec8e278
Vishal Aditya Kirtaniya <vishalkirtaniyaofficial@gmail.com>
2024-07-30 17:43:30 +0530

    addded current files that is prepared in the github

---
+++ src/components/navigationBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const NavigationBar = () => {
+  return (
+    <div className="h-20 bg-black  w-full flex justify-center items-center montserratFont">
+      <div className="h-[50px] w-[1440px]  flex justify-between items-center ">
+        <div className="h-[34px] w-[210px]  ">
+          <Image
+            src="/homepage/Straight logo 1.png"
+            height={50}
+            width={200}
+            alt="logo "
+          ></Image>
+        </div>
+
+        <nav className="h-full w-[1000px] navButtons  gap-[50px] flex justify-between items-center text-white font-[500] text-[16px] ">
+          <Link href={"#"}>Home</Link>
+          <Link href={"#"}>Find Job</Link>
+          <Link href={"#"}>Fing Talent</Link>
+          <Link href={"#"}>Post a job</Link>
+          <Link href={"#"}>Shortlist</Link>
+          <Link href={"#"}>Community</Link>
+          <Link
+            href={"#"}
+            className="h-[49px] w-[147px] flex justify-center items-center bg-customRed rounded-full "
+          >
+            Login/Signup
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        customRed: "rgba(187,54,42,1)",
+        customGrey: "#555658",
+        lightGrey: "#636a80",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
